a9bff1a8e54c6e6d0d8d0b5683a3b97ab8654264
Ada <139880994+Adamay01@users.noreply.github.com>
2023-12-15 17:24:40 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,65 @@
+
+def generate_random_data():
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+    main()
+def main():
+import random
+
+    data = generate_random_data()
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+def generate_random_data():
+import random
+def main():
+
+    return data
+
+    for item in data:
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+if __name__ == "__main__":
+    main()
+    data = generate_random_data()
+    main()
+if __name__ == "__main__":
+def main():
+    for item in data:
+def generate_random_data():
+
+
+    data = [random.randint(1, 100) for _ in range(10)]
+
+    return data
+import random
+        print(f"Random Number: {item}")        print(f"Random Number: {item}")
+def main():
+
+    data = generate_random_data()
+    for item in data:
+
+    main()
+if __name__ == "__main__":
+    return data
+    data = [random.randint(1, 100) for _ in range(10)]
+
+def generate_random_data():
+import random
+            System.out.println("Random Number: " + item);
+}
+        }
+
+public class RandomDataGenerator {
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+        }
+
+        int[] data = new int[10];
+        for (int i = 0; i < 10; i++) {
+    }
+    public static void main(String[] args) {
