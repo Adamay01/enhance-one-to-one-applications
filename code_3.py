b85f3c86ffe525fef3a193b393dac1e371beb4ba
Ada <139880994+Adamay01@users.noreply.github.com>
2023-12-15 20:27:00 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,39 @@
+    for item in data:
+import random
+
+        print(f"Random Number: {item}")
+def main():
+    main()
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+    data = generate_random_data()
+
+
+    return data    return data
+if __name__ == "__main__":
+        print(f"Random Number: {item}")
+    main()
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+
+def main():
+
+def generate_random_data():
+import random
+    data = generate_random_data()    return data
+    main()
+        print(f"Random Number: {item}")
+import random
+
+
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+    for item in data:
+def main():
+
+    data = generate_random_data()
+def generate_random_data():
